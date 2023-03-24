5d4547e2830bd12735d47094b27a5f1a5c781df6
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-03-24 10:57:17 +0600

    Added => unique key for mapping components

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Country from '../Country/Country';
+import './Countries.css'
+
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
@@ -10,12 +12,14 @@ const Countries = () => {
     }, [])
 
     return (
-        <div>
+        <div >
             <h1>Country: {countries.length}</h1>
-            {
-                countries.map(country => <Country country={country}>
-                    </Country>)
-            }
+            
+            <div className='countries-container'>
+                {
+                    countries.map(country => <Country country={country} key = {country.cca3}></Country>)
+                }
+            </div>
             
         </div>
     )
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './Country.css'
 
 const Country = (props) => {
-    const {name ,population, area, region} = props.country;
+    const {name ,population, area, region, flags} = props.country;
     return (
         <div className='country'>
-            <h2>country name: {name.common}</h2>
+            <h2>{name.common}</h2>
+            <img src={flags.png} alt="" />
             <p>Population: {population}</p>
             <p>Area: <small>{area}</small></p>
             <p><small>Region: {region}</small></p>
